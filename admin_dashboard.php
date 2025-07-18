7e3301ef42a13f58ed866dd8aa95d8aed683a6ee
Neelaka <neelakanadeeshan171@gmail.com>
2025-07-18 22:16:19 +0530

    Login and register page created

---
+++ admin_dashboard.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'admin') {
+    header("Location: login.php");
+    exit;
+}
+echo "Welcome Admin, " . $_SESSION['name'];
+echo "<br><a href='logout.php'>Logout</a>";
+?>
